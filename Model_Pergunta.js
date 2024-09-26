c280fd06177919a8169183cb5c7205812543a235
Yan Bueno <buenoy519@gmail.com>
2024-09-26 10:39:16 -0300

    Adição de coluna de telefone do usuário

--- Model_Pergunta.js
+++ Model_Pergunta.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const connection = require('./db_connection')
 
 const Pergunta = connection.define('table_perguntas0', {
-    assunto: {
+    username: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
